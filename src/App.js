f22be1072290c35cef1a2e675a6c0925c7bf4e33
Kelly Schumacher <kschumacher93@gmail.com>
2023-11-15 19:20:12 -0500

    added components needed to home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.scss';
 import HomePage from './pages/HomePage/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SideBar from "../src/components/SideBar/SideBar";
+import Header from "../src/components/Header/Header";
+import Footer from "../src/components/Footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" component={HomePage} />
-        {/* <Route path="" component={} /> */}
-        {/* <Route path="" component={} /> */}
-        {/* <Route path="" component={} /> */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+      <SideBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" component={HomePage} />
+          {/* <Route path="" component={} /> */}
+          {/* <Route path="" component={} /> */}
+          {/* <Route path="" component={} /> */}
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,16 @@
 import "./HomePage.scss";
+import RandomizedList from "../../components/RandomizedList/RandomizedList";
+import MostLiked from "../../components/MostLiked/MostLiked";
+import GenreList from "../../components/GenreList/GenreList";
+import MostCommented from "../../components/MostCommented/MostCommented";
 
 function HomePage() {
     return (
         <div className="HomePage">
-            <h1>HomePage</h1>
+            <MostCommented />
+            <RandomizedList />
+            <MostLiked />
+            <GenreList />          
         </div>
     );
 }
