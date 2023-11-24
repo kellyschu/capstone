caa50b8ef2b21ed514f6e96edf89a67eedb3136a
Kelly Schumacher <kschumacher93@gmail.com>
2023-11-23 22:05:54 -0500

    added footer links and centered

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.scss";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h6>Copyright © 2023 PODSTREAM. All rights reserved.</h6>
+            <div className="display-flex">
+                    <a href="https://kellyschudesigns.godaddysites.com/">CONTACT</a>
+                    <a href="https://www.linkedin.com/in/kellyschumacher/">LINKEDIN</a>
+                    <a href="https://github.com/kellyschu">GITHUB</a>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
