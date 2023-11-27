f43ce7d8aa68ff41a8b86b43488fae323f18e2df
Kelly Schumacher <kschumacher93@gmail.com>
2023-11-27 16:03:35 -0500

    added alternate coloring to episodes

--- src/components/EpisodeCard/EpisodeCard.js
+++ src/components/EpisodeCard/EpisodeCard.js
@@ -2,13 +2,16 @@ import "./EpisodeCard.scss";
 import { NavLink } from "react-router-dom";
 
 function EpisodeCard({ key, episodes }) {
+    const getCardColorClass = (index) => {
+        return index % 3 === 0 ? 'blue' : index % 3 === 1 ? 'grey' : 'white';
+    };
+    
     return (
         <>
             {episodes ? (
-                Object.values(episodes).map((episode) => (
+                Object.values(episodes).map((episode, index) => (
                     <NavLink to={`/episode/${episode.id}`} style= { { textDecoration: 'none' }} key={key}>
-                        <div className="episode-card">
-                        {/* <img src={placeholder} alt="img"/> */}
+                        <div className={`episode-card ${getCardColorClass(index)}`}>
                         <h2  className="episode-card__title">{episode.title.substring(0, 30)}{episode.title.length > 33 ? '...' : ''}</h2>
                         <p>{episode.channel}</p>
                         </div>
@@ -20,5 +23,6 @@ function EpisodeCard({ key, episodes }) {
         </>
     );
 }
+
 
 export default EpisodeCard;
